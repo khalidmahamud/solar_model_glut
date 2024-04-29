b2c1923777639ac96e3d092b6cf7c6018a9c6c24
khalidmahamud <22-46601-1@student.aiub.edu>
2024-04-30 02:17:55 +0600

    added star texture

--- solar_model.cpp
+++ solar_model.cpp
@@ -11,20 +11,22 @@ struct Planet {
     GLfloat rotationSpeed; // New field for rotation speed (day and night)
 };
 
-GLuint planetTextures[9]; // Array to store texture IDs for each planet
+GLuint textures[10]; // Array to store texture IDs for each planet
+
 GLUquadric *quad;
 
 void loadTextures() {
     // Load textures for each planet
-    planetTextures[0] = SOIL_load_OGL_texture("textures/mercury_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[1] = SOIL_load_OGL_texture("textures/venus_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[2] = SOIL_load_OGL_texture("textures/earth_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[3] = SOIL_load_OGL_texture("textures/mars_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[4] = SOIL_load_OGL_texture("textures/jupiter_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[5] = SOIL_load_OGL_texture("textures/saturn_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[6] = SOIL_load_OGL_texture("textures/uranus_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[7] = SOIL_load_OGL_texture("textures/neptune_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    planetTextures[8] = SOIL_load_OGL_texture("textures/sun_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[0] = SOIL_load_OGL_texture("textures/mercury_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[1] = SOIL_load_OGL_texture("textures/venus_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[2] = SOIL_load_OGL_texture("textures/earth_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[3] = SOIL_load_OGL_texture("textures/mars_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[4] = SOIL_load_OGL_texture("textures/jupiter_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[5] = SOIL_load_OGL_texture("textures/saturn_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[6] = SOIL_load_OGL_texture("textures/uranus_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[7] = SOIL_load_OGL_texture("textures/neptune_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[8] = SOIL_load_OGL_texture("textures/sun_texture.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+    textures[9] = SOIL_load_OGL_texture("textures/stars.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
 }
 
 Planet mercury = { 0, 0.05, 0.4, 1.0 / 88.0, 1.0 }; // 1/88 of Earth's orbit speed, 1000 times faster
@@ -35,7 +37,7 @@ Planet jupiter = { 4, 0.3, 2.5, 1.0 / 4333.0, 1.0 }; // 1/4333 of Earth's orbit 
 Planet saturn = { 5, 0.25, 3.5, 1.0 / 10759.0, 1.0 }; // 1/10759 of Earth's orbit speed, 1000 times faster
 Planet uranus = { 6, 0.2, 4.5, 1.0 / 30685.0, 1.0 }; // 1/30685 of Earth's orbit speed, 1000 times faster
 Planet neptune = { 7, 0.18, 5.5, 1.0 / 60190.0, 1.0 }; // 1/60190 of Earth's orbit speed, 1000 times faster
-Planet sun = { 8, 0.5, 0, 0.0, 0.0 }; // The sun doesn't move
+Planet sun = { 8, 0.3, 0, 0.0, 0.0 }; // The sun doesn't move
 
 GLfloat light_position[] = { 0.0, 0.0, 5.0, 0.0 }; // Adjust light position
 GLfloat light_ambient[] = { 0.2, 0.2, 0.2, 1.0 }; // Ambient light color
@@ -55,14 +57,9 @@ void init(void) {
     loadTextures();
     quad = gluNewQuadric();
 
-    float screenWidth = glutGet(GLUT_SCREEN_WIDTH);
-    float screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
-
-    float aspectRatio = screenWidth / screenHeight;
-
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glFrustum(-1 * aspectRatio, 1 * aspectRatio, -1, 1, 2, 15);
+    glFrustum(-1 * 1.7, 1 * 1.7, -1, 1, 2, 15);
     glMatrixMode(GL_MODELVIEW);
 
 
@@ -80,7 +77,7 @@ void init(void) {
 
 void drawPlanet(Planet planet, GLfloat angle, GLfloat rotationAngle) {
     glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, planetTextures[planet.id]); // Bind the texture for the current planet
+    glBindTexture(GL_TEXTURE_2D, textures[planet.id]); // Bind the texture for the current planet
 
     glPushMatrix();
     glRotatef(angle, 0.0, 0.0, 1.0); // Rotate around the sun
@@ -88,7 +85,7 @@ void drawPlanet(Planet planet, GLfloat angle, GLfloat rotationAngle) {
     glRotatef(rotationAngle, 0.0, 0.0, 1.0); // Rotate around its own axis (day and night)
     
     gluQuadricTexture(quad,1);
-    gluSphere(quad,planet.radius,20,20);
+    gluSphere(quad, planet.radius, 20, 20);
     
     glPopMatrix();
 
@@ -113,6 +110,24 @@ void drawOrbit(Planet planet) {
 
 
 void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    
+    gluLookAt(8, 0, 5, 2.5, 0, 0, 0, 1, 5);
+
+    // Draw background star texture
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, textures[9]); // Bind the stars texture
+
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-10.0f, -20.0f, 2.0f); // Bottom-left corner
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(10.0f, -10.0f, -1.0f); // Bottom-right corner
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(10.0f, 10.0f, -1.0f); // Top-right corner
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-10.0f, 10.0f, -1.0f); // Top-left corner
+    glEnd();
+
+    glDisable(GL_TEXTURE_2D); // Disable texture after drawing the background
+
     static GLfloat mercuryAngle = 0.0;
     static GLfloat venusAngle = 0.0;
     static GLfloat earthAngle = 0.0;
@@ -131,9 +146,6 @@ void display() {
     static GLfloat uranusRotationAngle = 0.0;
     static GLfloat neptuneRotationAngle = 0.0;
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    gluLookAt(8, 0, 5, 2.5, 0, 0, 0, 1, 5);
 
     // Update rotation angles
     mercuryAngle += mercury.orbitSpeed * 1000.0;
